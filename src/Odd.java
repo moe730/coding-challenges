7e0bb1e385483f0d2cf0742eeaf229b4ac97cd87
eddy <eddyv5413@gmail.com>
2022-08-03 10:59:39 -0400

    added one solution

---
+++ src/Odd.java
@@ -0,0 +1,17 @@
+public class Odd {
+
+    public static void isEven(int number){
+
+        if(number % 2 ==0){
+            System.out.println("your number is even");
+        }else
+            System.out.println("your number is odd");
+    }
+
+    public static void main(String[] args) {
+        //this is a test case
+        //todo create a driver class for these methods
+        isEven(6);
+    }
+
+}
